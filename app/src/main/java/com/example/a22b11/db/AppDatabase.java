2fe8ad8aa663215d977f55abe41826ffa0f1edc3
Ethan Trevor <ethan@nandlab.com>
2022-05-23 23:11:01 +0200

    Add activity and mood class as database entities

--- app/src/main/java/com/example/a22b11/db/AppDatabase.java
+++ app/src/main/java/com/example/a22b11/db/AppDatabase.java
@@ -4,8 +4,12 @@ import androidx.room.Database;
 import androidx.room.RoomDatabase;
 import androidx.room.TypeConverters;
 
-@Database(entities = {User.class}, version = 1)
+@Database(entities = {User.class, Activity.class, Mood.class}, version = 1)
 @TypeConverters({Converter.class})
 public abstract class AppDatabase extends RoomDatabase {
     public abstract UserDao userDao();
+
+    public abstract ActivityDao activityDao();
+
+    public abstract MoodDao moodDao();
 }
