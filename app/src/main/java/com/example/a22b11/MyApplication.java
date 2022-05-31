50c8c053d3ef4d3d181fe56d04dd8c903036ada7
MarvinKhal <103659386+MarvinKhal@users.noreply.github.com>
2022-05-31 21:57:12 +0200

    added foreground service and rollback service and added them into my application added 2 new permissions

--- app/src/main/java/com/example/a22b11/MyApplication.java
+++ app/src/main/java/com/example/a22b11/MyApplication.java
@@ -1,10 +1,14 @@
 package com.example.a22b11;
 
+import android.app.ActivityManager;
 import android.app.Application;
+import android.content.Context;
+import android.content.Intent;
 
 import androidx.room.Room;
 
 import com.example.a22b11.db.AppDatabase;
+import com.example.a22b11.MyForegroundService;
 
 public class MyApplication extends Application {
 
@@ -17,6 +21,26 @@ public class MyApplication extends Application {
         instance = this;
         appDatabase = Room.databaseBuilder(getApplicationContext(),
                 AppDatabase.class, "database-name").build();
+
+        //TODO check implementation
+        //Foreground service
+        //setContentView(R.layout.activity_main);
+        if(!foregroundServiceRunning()) {
+            Intent serviceIntent = new Intent(this, MyForegroundService.class);
+            startForegroundService(serviceIntent);
+        }
+    }
+
+    public boolean foregroundServiceRunning(){
+        ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for(ActivityManager.RunningServiceInfo service: activityManager.getRunningServices(Integer.MAX_VALUE)){
+            if(MyForegroundService.class.getName().equals(service.service.getClassName())) {
+                return true;
+
+            }
+        }
+
+        return false;
     }
 
     public static MyApplication getInstance() {
