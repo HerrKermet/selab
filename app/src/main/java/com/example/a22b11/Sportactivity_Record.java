007e3fec30ea5d2d33d04fb2cd4fe7283b7eb27d
DavidStec <david.stechow@rub.de>
2022-06-17 14:15:04 +0200

    Changed Sportactivity_Record buttons

--- app/src/main/java/com/example/a22b11/Sportactivity_Record.java
+++ app/src/main/java/com/example/a22b11/Sportactivity_Record.java
@@ -1,8 +1,10 @@
 package com.example.a22b11;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.view.View;
@@ -13,14 +15,28 @@ public class Sportactivity_Record extends AppCompatActivity {
 
     Chronometer chronometer;
     private boolean running = false;
-    Button Start_Button, Stop_Button;
+    Button button, finishButton;
     private long pauseOffset;
+    int buttonState = 0;  // initial state  0 = ready to start / 1 = ready to stop / 2 = stopped ready to resume or finish
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sportrecord);
+        button = findViewById(R.id.Start_Button);
+        finishButton = findViewById(R.id.button20);
+
+
+
+
+
+
+
+        if (buttonState != 2) {
+            finishButton.setVisibility(View.GONE);
+        }
+        else finishButton.setVisibility(View.VISIBLE);
     }
 
 
@@ -28,9 +44,41 @@ public class Sportactivity_Record extends AppCompatActivity {
 
 
 
+    public void toggleButton(View view)
+    {
+        if (buttonState == 0) {  // ready to start
+            button.setText(R.string.Stop);  // change to stop
+            finishButton.setVisibility(View.GONE);
+
+            startTimer(view);
+
+            buttonState = 1;
+        }
+
+        else if (buttonState == 1) { // ready to stop
+            button.setText(R.string.resume); // change to resume
+
+            finishButton.setVisibility(View.VISIBLE);
+            stopTimer(view);
+
+            buttonState = 2;
+        }
+
+        else if (buttonState == 2) { // is stopped and ready to resume or finish
+            button.setText(R.string.Stop);  // change to stop
+            finishButton.setVisibility(View.GONE);
+
+
+            startTimer(view);
+
+            buttonState = 1;
+        }
+
+    }
+
     public void startTimer(View view){
         chronometer = findViewById(R.id.Chronometer);
-        Start_Button = (Button) findViewById(R.id.Start_Button);
+
 
         if(!running){
             chronometer.setBase(SystemClock.elapsedRealtime() - pauseOffset);
@@ -40,9 +88,8 @@ public class Sportactivity_Record extends AppCompatActivity {
 
     }
 
-    public void StopTimer(View view) {
+    public void stopTimer(View view) {
         chronometer = findViewById(R.id.Chronometer);
-        Stop_Button = (Button) findViewById(R.id.Pause_Button);
 
         if (running) {
             chronometer.stop();
