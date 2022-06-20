f8cf82566de3bd223b509fdc6df6c938c69d9af4
ABlessingFromTheLord <94542984+ABlessingFromTheLord@users.noreply.github.com>
2022-06-20 20:15:24 +0200

    Added Sportactivity_Edit_Selection

---
+++ app/src/main/java/com/example/a22b11/Sportactivity_Edit_Selection.java
@@ -0,0 +1,14 @@
+package com.example.a22b11;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Sportactivity_Edit_Selection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sportedit_selection);
+    }
+}
