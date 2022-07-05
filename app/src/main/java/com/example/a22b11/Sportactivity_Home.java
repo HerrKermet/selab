1fe3f741e0cee1d6ac8c79a91171850ec9a70c50
Kuzzi04 <philipp.kusmierz@gmail.com>
2022-07-05 16:49:40 +0200

    Added BarChart into Sportactivity_Home.java Added BarChart implementation into Sportactivity_Home.java Fixed Bug Constraints in activity_sporthome.xml

--- app/src/main/java/com/example/a22b11/Sportactivity_Home.java
+++ app/src/main/java/com/example/a22b11/Sportactivity_Home.java
@@ -3,13 +3,11 @@ package com.example.a22b11;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -18,15 +16,28 @@ import com.example.a22b11.adapter.itemAdapter;
 import com.example.a22b11.db.Activity;
 import com.example.a22b11.db.ActivityDao;
 import com.example.a22b11.db.AppDatabase;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.formatter.ValueFormatter;
 import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 
+//libraries for BarChart
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
 import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
 
 public class Sportactivity_Home extends AppCompatActivity {
 
@@ -47,7 +58,38 @@ public class Sportactivity_Home extends AppCompatActivity {
 
         setContentView(R.layout.activity_sporthome);
 
+        //BarChart
+        {
+            BarChart barChart = findViewById(R.id.barChart);
 
+            List<BarEntry> entries = new ArrayList<>();
+            LocalDate [] dates = exampleDates();        //Later from database
+            long [] durations = exampleDurations();     //Later from database
+            fillYValues(entries, dates, durations);
+            /*
+            fills y-values with duration and sets x-values with
+            numbers in float from 0-<days between start and enddate>
+            */
+
+
+            BarDataSet set = new BarDataSet(entries, "Data");
+
+            set.setColors(ColorTemplate.MATERIAL_COLORS);
+            set.setValueTextColor(android.R.color.black);
+            //set.setValueTextSize(16f);
+
+            BarData barData = new BarData(set);
+
+            barChart.setFitBars(true);
+            barChart.setData(barData); //If no data is available a message is on the screen: "No chart data available"
+            barChart.getDescription().setText("Bar Chart tolli");
+            barChart.animateY(2000);
+            fillXValues(barChart, dates[0], entries.size());
+            /*
+            replaces numbers of x-values with dates from start to enddate
+             */
+
+        }
         textViewRecentActivities = findViewById(R.id.textView31);
 
 
@@ -124,12 +166,83 @@ public class Sportactivity_Home extends AppCompatActivity {
 
         //done with database query
 
-
         //TODO  activitiesBetween ist initialisiert und die activities sollten aufsteigend sortiert sein
         //TODO check if list is null
 
 
 
+    }
+
+    private void fillYValues(List<BarEntry> entryList, LocalDate[] dates, long [] durations) {
+        long [] mergedResults = mergeActivities(dates, durations);
+        LocalDate startDate = dates[0];
+        List<LocalDate> mergedDates = dateList(startDate, startDate.plusDays(mergedResults.length));
+        for(int i=0; i<mergedResults.length; i++){
+            entryList.add(new BarEntry((float) i,mergedResults[i]));
+        }
+    }
+
+    private void fillXValues(BarChart chart, LocalDate startDate, int length){
+
+        String[] naming = new String[length+1];
+        for (int i = 0; i<length; i++){
+            naming[i] = String.valueOf(startDate.plusDays(i).format(DateTimeFormatter.ofPattern("dd.MM")));
+        }
+
+        ValueFormatter formatter = new ValueFormatter() {
+            @Override
+            public String getAxisLabel(float value, AxisBase axis) {
+                return naming[(int) value];
+            }
+        };
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setGranularity(1f);
+        xAxis.setValueFormatter(formatter);
+    }
+    public static List<LocalDate> dateList(LocalDate startDate, LocalDate endDate) {
+
+        System.out.println("Starting from " + startDate);
+        System.out.println("Ending at " + endDate);
+
+        List<LocalDate> days = new ArrayList<>(25);
+        while (startDate.isBefore(endDate) || startDate.equals(endDate)) {
+            days.add(startDate);
+            startDate = startDate.plusDays(1);
+        }
+        return days;
+    }
+    /*
+    mergeActivities below sums up the duration of activities
+    that happened on the same day.
+    returns long-Array with durations only
+     */
+    public static long[] mergeActivities(LocalDate[] dates, long[] duration){
+        LocalDate startDate = dates[0];
+        LocalDate endDate = dates[dates.length-1];
+        int daysBetween = (int) ChronoUnit.DAYS.between(startDate, endDate);
+        long result [] = new long[daysBetween+1];
+
+
+        for(int i = 0; i < result.length; i++){
+            for(int m = 0; m < dates.length; m++){
+                if(startDate.plusDays(i).isEqual(dates[m])) result[i]+=duration[m];
+            }
+        }
+        return result;
+    }
+    /*
+    testing later we won't need that. it's only for testing.
+    creates two arrays, one LocalDate (dates), one long (durations on each date)
+
+     */
+
+        public static LocalDate[] exampleDates () {
+        LocalDate[] dates = {LocalDate.parse("1970-01-01"), LocalDate.parse("1970-01-02"), LocalDate.parse("1970-01-04"), LocalDate.parse("1970-01-04"), LocalDate.parse("1970-01-06"), LocalDate.parse("1970-01-07"), LocalDate.parse("1970-01-11"), LocalDate.parse("1970-01-12"), LocalDate.parse("1970-01-13"), LocalDate.parse("1970-01-14"), LocalDate.parse("1970-01-17"), LocalDate.parse("1970-01-18")};
+        return dates;
+    }
+        public static long[] exampleDurations () {
+        long[] durations = {4, 10, 34, 1, 10, 2, 5, 6, 8, 4, 3, 9};
+        return durations;
     }
 
     @Override
