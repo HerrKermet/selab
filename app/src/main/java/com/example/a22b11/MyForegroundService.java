bbdcff228823997ac2595990d270941b782969a3
MarvinKhal <103659386+MarvinKhal@users.noreply.github.com>
2022-07-13 12:21:56 +0200

    Automatic detection of an activity and pauses

--- app/src/main/java/com/example/a22b11/MyForegroundService.java
+++ app/src/main/java/com/example/a22b11/MyForegroundService.java
@@ -12,32 +12,56 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.IBinder;
 import android.util.Log;
-import android.widget.Toast;
 
 
 import androidx.annotation.Nullable;
 
 import com.example.a22b11.db.Activity;
+import com.example.a22b11.db.ActivityDao;
+import com.example.a22b11.db.AppDatabase;
+import com.google.common.util.concurrent.ListenableFuture;
 
+import java.time.Duration;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 import java.util.Timer;
 import java.util.TimerTask;
 
 
 public class MyForegroundService extends Service  {
+
+    int stepThreshholdToTriggerActivity = 50; //TODO adjust value
+    int maxPauseDurationSeconds = 300;  //TODO adjust value
+    int arraySize = 300;                  //TODO adjust value
+
+    AppDatabase db;
+
+
+
     int counter = 0;
     int threshold = 150;
     int threshholddelay = 16;
     Accelerometer accelerometer = new Accelerometer();
     int steps = accelerometer.getStepCount();
     int stepCount;
+    int activityStepCount;
+    int pauseDurationSeconds = 0;
     double MagnitudePrevious;
-    boolean recording = false;
+    boolean isRecording = false;
+    boolean isPause = false;
+
     int CounterforMedian = 0;
+
+    int indexCounter = 0;
+    int indexTimerClock = 0;
+
+    int [] scanArray = new int[arraySize];
+
+    Activity activity = new Activity(); // activity which gets recorded
+
 
     List RawAccDataX = new LinkedList<Integer>();
     List RawAccDataY = new LinkedList<Integer>();
@@ -46,12 +70,14 @@ public class MyForegroundService extends Service  {
     SensorManager sensorManager;
     Sensor sensor;
 
+    //TODO DELETE SOME LOGS
     @Override
     public void onCreate() {
         super.onCreate();
 
         SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        db = ((MyApplication)getApplication()).getAppDatabase();
 
         SensorEventListener stepDetector = new SensorEventListener() {
             @Override
@@ -62,7 +88,7 @@ public class MyForegroundService extends Service  {
                     float z_acceleration = sensorEvent.values[2];
 
                     double Magnitude = Math.sqrt(
-                                    x_acceleration * x_acceleration +
+                            x_acceleration * x_acceleration +
                                     y_acceleration * y_acceleration +
                                     z_acceleration * z_acceleration);
                     double MagnitudeDelta = Magnitude - MagnitudePrevious;
@@ -70,6 +96,8 @@ public class MyForegroundService extends Service  {
 
                     if (MagnitudeDelta > 10) {
                         stepCount++;
+                        activityStepCount++;
+                        Log.d("activity sensor","current stepcount in this interval " + activityStepCount);
                     }
                 }
             }
@@ -100,29 +128,70 @@ public class MyForegroundService extends Service  {
                 //and each 60 seconds the Raw data is summarized and saved
                 CounterforMedian +=1;
 
+
                 if (CounterforMedian == 60){
                     CounterforMedian = 0;
                 }
 
 
-                Log.d("medianCouneter", String.valueOf(CounterforMedian));
-                if (!recording) counter+=1;
-                if (counter ==  threshholddelay) {
-                    counter = 0 ;
-                    if (stepCount >= threshold){
-                        recording = true;
-                        //saving of the accelerometer raw data summarized as median
-
-                        // TODO:start activity and add the steps onto the newly started activity
-                        Instant instant = Instant.now();
-                        Activity activity = new Activity();
-                        activity.start = instant.minus( threshholddelay-1, ChronoUnit.SECONDS);
+                // start of functionality for scanArray
+                if (indexTimerClock % 10 == 0){
+                    Log.d("activity sensor", "reached 10 second threshhold");
+                    scanArray[indexCounter] = activityStepCount;
+                    indexCounter = (indexCounter + 1) % arraySize;  // increase index every 10 seconds to fill array with stepcounts
+                    activityStepCount = 0;
+                    int sum = Arrays.stream(scanArray).sum();
 
 
-                    } else{
-                        stepCount = 0;
+                    //TODO delete this
+                    String arrContent = "";
+                    for (int count :
+                            scanArray) {
+                        arrContent += " " + count;
+                    }
+
+                    Log.d("activity sensor", "sum of current array is " + sum + " and array is " + arrContent);
+
+
+
+                    if (sum >= stepThreshholdToTriggerActivity && !isRecording) {
+                        startRecordingActivity();
+                        isRecording = true;
+
+                        Log.d("activity sensor","started new activity with number of steps" + sum);
+
+
+                    }
+
+                    if (sum < stepThreshholdToTriggerActivity && isRecording) {
+                        if (isPause) {
+                            // check how long  pause
+                            pauseDurationSeconds += 10;
+                            Log.d("activity sensor", "pause duration currently at " + pauseDurationSeconds);
+                            if(pauseDurationSeconds >= maxPauseDurationSeconds) {  // if pause duration is greater than threshhold of maxPauseDuration
+                                endRecordingActivity();
+                            }
+
+                        }
+                        else{
+                            Log.d("activity sensor", "detected start of pause");
+                            isPause = true;
+                            pauseDurationSeconds = 0;
+                        }
+
                     }
                 }
+
+                Log.d("activity sensor", "increasing timer clock to: " + indexTimerClock);
+                indexTimerClock++;
+                if(indexTimerClock == 1000) indexTimerClock = 0;
+                // end of functionality for scanArray
+
+
+
+
+                Log.d("medianCouneter", String.valueOf(CounterforMedian));
+
             }
         };
         //Timer start
@@ -169,6 +238,32 @@ public class MyForegroundService extends Service  {
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    public void startRecordingActivity() {
+        // init new activity
+        activity = new Activity();
+
+        activity.start = Instant.now();
+        activity.isAutomaticallyDetected = true;
+        activity.activityType = Activity.ActivityType.valueOf("OTHER");  //TODO check implementation
+        activity.type = getString(R.string.autoGenerated);
+
+        Log.d("activity sensor","set activityType to" + activity.activityType);
+    }
+
+    public void endRecordingActivity() {
+        // end recording activity and save to database
+        isRecording = false;
+        isPause = false;
+
+        activity.end = Instant.now().minus(5, ChronoUnit.MINUTES);
+        long duration = Duration.between(activity.start, activity.end).getSeconds();
+        activity.duration = Math.toIntExact(duration);
+
+        ActivityDao activityDao = db.activityDao();
+        ListenableFuture<Long> asyncAccess = activityDao.insert(activity);
+        Log.d("activity sensor","saved activity with duration of " + duration + " seconds to database");
     }
 
 }
