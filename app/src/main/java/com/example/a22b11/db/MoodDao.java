c778bfd64069204d41fbc0b90f291fda0443ae6a
Ethan Trevor <ethan@nandlab.com>
2022-07-31 21:34:16 +0200

    StatisticalRepresentation: get users and moods by correct user ID

--- app/src/main/java/com/example/a22b11/db/MoodDao.java
+++ app/src/main/java/com/example/a22b11/db/MoodDao.java
@@ -34,6 +34,12 @@ public interface MoodDao {
     @Query("SELECT * FROM moods WHERE assessment BETWEEN :assessMin AND :assessMax ORDER BY assessment ASC")
     ListenableFuture<List<Mood>> getMoodBetween(Instant assessMin, Instant assessMax);
 
+    @Query("SELECT * FROM moods WHERE user_id = :userId AND assessment BETWEEN :assessMin AND :assessMax ORDER BY assessment ASC")
+    ListenableFuture<List<Mood>> getUserMoodBetween(long userId, Instant assessMin, Instant assessMax);
+
+    @Query("SELECT * FROM moods WHERE user_id = :userId AND assessment BETWEEN :assessMin AND :assessMax ORDER BY assessment ASC")
+    List<Mood> getUserMoodBetweenSync(long userId, Instant assessMin, Instant assessMax);
+
     @Query("SELECT * FROM moods WHERE user_id = :userId AND id IS NULL")
     List<Mood> getNewByUserIdSync(long userId);
 
